20473bfb5af21dd2af6a933ab2cb176c40f9f571
Volodymyr Kuzmenko <kuzmenko.vladimir22@gmail.com>
2025-09-24 16:03:57 +0300

    data layer added

--- src/main/java/com/epam/rd/autocode/spring/project/model/Book.java
+++ src/main/java/com/epam/rd/autocode/spring/project/model/Book.java
@@ -1,5 +1,52 @@
 package com.epam.rd.autocode.spring.project.model;
 
+import com.epam.rd.autocode.spring.project.model.enums.AgeGroup;
+import com.epam.rd.autocode.spring.project.model.enums.Language;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "BOOKS")
 public class Book {
-    // TODO Place your code here
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String genre;
+
+    @Enumerated(EnumType.STRING)
+    private AgeGroup ageGroup;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "publication_year")
+    private LocalDate publicationDate;
+
+    @Column
+    private String author;
+
+    @Column(name = "number_of_pages")
+    private Integer pages;
+
+    @Column
+    private String characteristics;
+
+    @Column
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Language language;
 }
--- src/main/java/com/epam/rd/autocode/spring/project/model/User.java
+++ src/main/java/com/epam/rd/autocode/spring/project/model/User.java
@@ -1,5 +1,25 @@
 package com.epam.rd.autocode.spring.project.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    // TODO Place your code here
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(unique = true, name = "NAME")
+    private String name;
 }
